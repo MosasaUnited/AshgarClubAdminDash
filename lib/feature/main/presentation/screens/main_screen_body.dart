10c6546c0e287d1831e8592afb55bb28b4fc44f7
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-09-01 00:11:38 +0300

    AddAppLogoAsLeadingInAppBar

--- lib/feature/main/presentation/screens/main_screen_body.dart
+++ lib/feature/main/presentation/screens/main_screen_body.dart
@@ -1,3 +1,4 @@
+import 'package:ashgar_club_admin_dash/core/constants/icon_assets.dart';
 import 'package:ashgar_club_admin_dash/feature/football_courts/presentation/football_courts_view.dart';
 import 'package:ashgar_club_admin_dash/feature/home/presentation/home_view.dart';
 import 'package:ashgar_club_admin_dash/feature/main/presentation/screens/side_menu_screen.dart';
@@ -38,49 +39,54 @@ class _MainScreenBodyState extends State<MainScreenBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            if (constraints.maxWidth > 600) {
-              // Desktop Or Web
-              return Row(
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: SizedBox(
-                      child: SideMenuScreen(onItemTapped: _onItemTapped),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 7,
-                    child: SizedBox(
-                      child: _screens[_selectedIndex],
-                    ),
-                  ),
-                ],
-              );
-            } else {
-              return Stack(
-                children: [
-                  _screens[_selectedIndex],
-                  Positioned(
-                      top: 0,
-                      left: 0,
-                      child: IconButton(
-                        onPressed: () {
-                          Scaffold.of(context).openDrawer();
-                        },
-                        icon: const Icon(Icons.menu),
-                      ))
-                ],
-              );
-            }
-          },
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Image.asset(IconAssets.appLogo),
         ),
-      ),
-      drawer: Drawer(
-        child: SideMenuScreen(onItemTapped: _onItemTapped),
+        body: SafeArea(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              if (constraints.maxWidth > 600) {
+                // Desktop Or Web
+                return Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: SizedBox(
+                        child: SideMenuScreen(onItemTapped: _onItemTapped),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 7,
+                      child: SizedBox(
+                        child: _screens[_selectedIndex],
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                return Stack(
+                  children: [
+                    _screens[_selectedIndex],
+                    Positioned(
+                        top: 0,
+                        left: 0,
+                        child: IconButton(
+                          onPressed: () {
+                            Scaffold.of(context).openDrawer();
+                          },
+                          icon: const Icon(Icons.menu),
+                        ))
+                  ],
+                );
+              }
+            },
+          ),
+        ),
+        drawer: Drawer(
+          child: SideMenuScreen(onItemTapped: _onItemTapped),
+        ),
       ),
     );
   }
