66ea97284ec4e84e091275469ea598c46e5117fd
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-09-03 22:33:22 +0300

    StartingFootballCourtsScreen

---
+++ lib/feature/football_courts/presentation/screens/football_court_three.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FootballCourtThree extends StatelessWidget {
+  const FootballCourtThree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/feature/football_courts/presentation/screens/football_courts_view_body.dart
+++ lib/feature/football_courts/presentation/screens/football_courts_view_body.dart
@@ -1,17 +1,91 @@
+import 'package:ashgar_club_admin_dash/feature/football_courts/presentation/screens/football_court_one.dart';
+import 'package:ashgar_club_admin_dash/feature/football_courts/presentation/screens/football_court_three.dart';
+import 'package:ashgar_club_admin_dash/feature/football_courts/presentation/screens/football_court_two.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:segmented_button_slide/segmented_button_slide.dart';
 
-import '../../../../core/theme/styles.dart';
+import '../../../../core/theme/colors.dart';
 
-class FootballCourtsViewBody extends StatelessWidget {
+class FootballCourtsViewBody extends StatefulWidget {
   const FootballCourtsViewBody({super.key});
 
   @override
+  State<FootballCourtsViewBody> createState() => _FootballCourtsViewBodyState();
+}
+
+class _FootballCourtsViewBodyState extends State<FootballCourtsViewBody> {
+  int selectedOption = 0;
+
+  void toggleFootballCourt() {
+    setState(() {
+      selectedOption = (selectedOption + 1) % 3;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Football View',
-        style: Styles.textStyle30,
-      ),
+    return Column(
+      children: [
+        SegmentedButtonSlide(
+          selectedEntry: selectedOption,
+          onChange: (selected) => setState(
+            () => selectedOption = selected,
+          ),
+          curve: Curves.easeInOut,
+          entries: const [
+            SegmentedButtonSlideEntry(
+              label: 'Court 1',
+            ),
+            SegmentedButtonSlideEntry(
+              label: 'Court 2',
+            ),
+            SegmentedButtonSlideEntry(
+              label: 'Court 3',
+            ),
+          ],
+          colors: const SegmentedButtonSlideColors(
+            barColor: Colors.transparent,
+            backgroundSelectedColor: Colors.transparent,
+          ),
+          slideShadow: const [
+            BoxShadow(
+              color: MyColors.myTeal,
+            ),
+          ],
+          margin: const EdgeInsets.all(8),
+          height: 50.h,
+          padding: const EdgeInsets.all(8),
+          borderRadius: BorderRadius.circular(16),
+          selectedTextStyle: TextStyle(
+            fontWeight: FontWeight.w900,
+            fontSize: 13.sp,
+            fontStyle: FontStyle.italic,
+          ),
+          unselectedTextStyle: TextStyle(
+            fontWeight: FontWeight.w700,
+            color: MyColors.myTeal,
+            fontSize: 12.sp,
+          ),
+          hoverTextStyle: TextStyle(
+            color: Colors.yellow,
+            fontSize: 11.sp,
+          ),
+        ),
+        SizedBox(
+          height: 20.h,
+        ),
+        Column(
+          children: [
+            if (selectedOption == 0)
+              const FootballCourtOne()
+            else if (selectedOption == 1)
+              const FootballCourtTwo()
+            else
+              const FootballCourtThree()
+          ],
+        ),
+      ],
     );
   }
 }
