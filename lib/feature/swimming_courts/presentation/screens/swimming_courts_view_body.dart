574c85c007d458488d3341576c1d3e70eecd1cee
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-09-05 11:50:53 +0300

    StartingSwimmingPoolScreens

--- lib/feature/swimming_courts/presentation/screens/swimming_courts_view_body.dart
+++ lib/feature/swimming_courts/presentation/screens/swimming_courts_view_body.dart
@@ -1,17 +1,93 @@
+import 'package:ashgar_club_admin_dash/feature/swimming_courts/presentation/screens/swimming_pool_one.dart';
+import 'package:ashgar_club_admin_dash/feature/swimming_courts/presentation/screens/swimming_pool_three.dart';
+import 'package:ashgar_club_admin_dash/feature/swimming_courts/presentation/screens/swimming_pool_two.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:segmented_button_slide/segmented_button_slide.dart';
 
-import '../../../../core/theme/styles.dart';
+import '../../../../core/theme/colors.dart';
 
-class SwimmingCourtsViewBody extends StatelessWidget {
+class SwimmingCourtsViewBody extends StatefulWidget {
   const SwimmingCourtsViewBody({super.key});
 
   @override
+  State<SwimmingCourtsViewBody> createState() => _SwimmingCourtsViewBodyState();
+}
+
+class _SwimmingCourtsViewBodyState extends State<SwimmingCourtsViewBody> {
+  int selectedOption = 0;
+
+  void toggleFootballCourt() {
+    setState(() {
+      selectedOption = (selectedOption + 1) % 3;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Swimming Courts View',
-        style: Styles.textStyle30,
-      ),
+    return Column(
+      children: [
+        SegmentedButtonSlide(
+          selectedEntry: selectedOption,
+          onChange: (selected) => setState(
+            () => selectedOption = selected,
+          ),
+          curve: Curves.bounceInOut,
+          entries: const [
+            SegmentedButtonSlideEntry(
+              label: 'Pool 1',
+            ),
+            SegmentedButtonSlideEntry(
+              label: 'Pool 2',
+            ),
+            SegmentedButtonSlideEntry(
+              label: 'Pool 3',
+            ),
+          ],
+          colors: const SegmentedButtonSlideColors(
+            barColor: Colors.transparent,
+            backgroundSelectedColor: Colors.transparent,
+          ),
+          slideShadow: const [
+            BoxShadow(
+              color: MyColors.myLightBlue,
+            ),
+          ],
+          margin: const EdgeInsets.all(8),
+          height: 50.h,
+          padding: const EdgeInsets.all(8),
+          borderRadius: BorderRadius.circular(16),
+          selectedTextStyle: TextStyle(
+            fontWeight: FontWeight.w900,
+            fontSize: 13.sp,
+            fontStyle: FontStyle.italic,
+          ),
+          unselectedTextStyle: TextStyle(
+            fontWeight: FontWeight.w700,
+            color: MyColors.myTeal,
+            fontSize: 12.sp,
+          ),
+          hoverTextStyle: TextStyle(
+            color: Colors.redAccent,
+            fontSize: 11.sp,
+          ),
+        ),
+        SizedBox(
+          height: 20.h,
+        ),
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              if (selectedOption == 0)
+                const SwimmingPoolOne()
+              else if (selectedOption == 1)
+                const SwimmingPoolTwo()
+              else
+                const SwimmingPoolThree()
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/feature/swimming_courts/presentation/screens/swimming_pool_one.dart
@@ -0,0 +1,234 @@
+import 'package:data_table_2/data_table_2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import '../../../../core/theme/styles.dart';
+
+class SwimmingPoolOne extends StatefulWidget {
+  const SwimmingPoolOne({super.key});
+
+  @override
+  State<SwimmingPoolOne> createState() => _SwimmingPoolOneState();
+}
+
+class _SwimmingPoolOneState extends State<SwimmingPoolOne> {
+  DateTime _selectedDay = DateTime.utc(2024, 9, 20);
+  CalendarFormat _calendarFormat = CalendarFormat.week;
+
+  final Map<DateTime, List<Map<String, dynamic>>> _events = {
+    DateTime.utc(2024, 9, 21): [
+      {'time': '10:00 AM', 'status': 'available'},
+      {'time': '11:00 AM', 'status': 'booked', 'paid': true},
+      {'time': '12:00 PM', 'status': 'booked', 'paid': false},
+    ],
+    DateTime.utc(2024, 9, 22): [
+      {'time': '09:00 AM', 'status': 'available'},
+      {'time': '10:00 AM', 'status': 'booked', 'paid': true},
+      {'time': '11:00 AM', 'status': 'available'},
+    ],
+    DateTime.utc(2024, 9, 23): [
+      {'time': '09:00 AM', 'status': 'available'},
+      {'time': '10:00 AM', 'status': 'booked', 'paid': true},
+      {'time': '11:00 AM', 'status': 'available'},
+    ],
+    DateTime.utc(2024, 9, 25): [
+      {'time': '09:00 AM', 'status': 'available'},
+      {'time': '10:00 AM', 'status': 'booked', 'paid': true},
+      {'time': '11:00 AM', 'status': 'available'},
+    ],
+  };
+
+  List<Map<String, dynamic>> _generateDailyHours() {
+    List<Map<String, dynamic>> hours = [];
+    for (int i = 0; i < 24; i++) {
+      final hour = i.toString().padLeft(2, '0');
+      const minute = '00';
+      final period = i < 12 ? 'AM' : 'PM';
+      hours.add({
+        'time': '$hour:$minute $period',
+        'status': 'available',
+      });
+    }
+    return hours;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 500.h,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Availability Calendar
+                  TableCalendar(
+                    headerStyle: HeaderStyle(
+                      titleCentered: true,
+                      formatButtonVisible: false,
+                      titleTextStyle: Styles.textStyle6.copyWith(
+                        fontSize: 8.sp,
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                    focusedDay: _selectedDay,
+                    firstDay: DateTime.utc(2024, 9, 19),
+                    lastDay: DateTime.utc(2024, 12, 31),
+                    calendarFormat: _calendarFormat,
+                    currentDay: DateTime.now(),
+                    onDaySelected: (selectedDay, focusedDay) {
+                      setState(() {
+                        _selectedDay = selectedDay;
+                      });
+                    },
+                    onFormatChanged: (format) {
+                      setState(() {
+                        _calendarFormat = format;
+                      });
+                    },
+                    calendarBuilders: CalendarBuilders(
+                      defaultBuilder: (context, day, focusedDay) {
+                        return Container(
+                          margin: const EdgeInsets.all(4.0),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: _events[day]?.any((event) =>
+                                        event['status'] == 'booked') ??
+                                    false
+                                ? Colors.red
+                                : Colors.yellow,
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: Text(
+                            day.day.toString(),
+                            style: const TextStyle(color: Colors.black),
+                          ),
+                        );
+                      },
+                      selectedBuilder: (context, day, focusedDay) {
+                        return Container(
+                          margin: const EdgeInsets.all(4.0),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: Text(
+                            day.day.toString(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Expanded(
+                    child: DataTable2(
+                      dataTextStyle: Styles.textStyle6.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                      border: TableBorder.all(),
+                      columns: [
+                        DataColumn(
+                          label: Center(
+                            child: Text(
+                              'Time',
+                              style: Styles.textStyle6.copyWith(
+                                fontSize: 7.sp,
+                                color: Colors.deepPurpleAccent,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Center(
+                            child: Text(
+                              'Status',
+                              style: Styles.textStyle6.copyWith(
+                                fontSize: 7.sp,
+                                color: Colors.deepPurpleAccent,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Center(
+                            child: Text(
+                              'Payment',
+                              style: Styles.textStyle6.copyWith(
+                                fontSize: 7.sp,
+                                color: Colors.deepPurpleAccent,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                      rows: List<DataRow>.generate(
+                        24,
+                        (index) {
+                          final hour = _generateDailyHours()[index];
+                          final event = _events[_selectedDay]?.firstWhere(
+                            (e) => e['time'] == hour['time'],
+                            orElse: () => hour,
+                          );
+                          return DataRow(cells: [
+                            DataCell(
+                              Text(
+                                DateFormat('h:mm a').format(
+                                  DateFormat('HH:mm').parse(hour['time']),
+                                ),
+                              ),
+                            ),
+                            DataCell(
+                              Text(
+                                event?['status'] == 'available'
+                                    ? 'Available'
+                                    : 'Booked',
+                                style: TextStyle(
+                                  color: event?['status'] == 'available'
+                                      ? Colors.green
+                                      : Colors.red,
+                                  fontSize: 6.sp,
+                                ),
+                              ),
+                            ),
+                            DataCell(event?['status'] == 'booked'
+                                ? Text(
+                                    event?['paid'] ? 'Paid' : 'Unpaid',
+                                    style: TextStyle(
+                                      color: event?['paid']
+                                          ? Colors.green
+                                          : Colors.red,
+                                      fontSize: 6.sp,
+                                    ),
+                                  )
+                                : event?['status'] == 'Available'
+                                    ? const Text('Paid')
+                                    : const Text('Not Paid')),
+                          ]);
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
